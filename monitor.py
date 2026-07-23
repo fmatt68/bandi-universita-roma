aa7f91720b3e464fd535fac4373a287f55c6269c
fmatt68 <fmattei68@hotmail.com>
2026-07-23 15:22:39 +0200

    Refactor monitor.py to extract links and simplify logic
    Removed priority and exclusion lists, added link extraction.

--- monitor.py
+++ monitor.py
@@ -1,45 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
-
-PRIORITA_ALTA = [
-    "ematologia",
-    "oncologia",
-    "immunologia",
-    "genetica",
-    "genetica medica",
-    "biologia",
-    "biologia molecolare",
-    "biologia cellulare",
-    "scienze biologiche",
-    "biotecnologie"
-]
-
-PRIORITA_MEDIA = [
-    "biochimica",
-    "patologia generale",
-    "medicina di laboratorio",
-    "tecniche di laboratorio biomedico"
-]
-
-PRIORITA_BASSA = [
-    "reumatologia",
-    "fisiologia",
-    "istologia"
-]
-
-ESCLUSIONI = [
-    "infermieristica",
-    "fisioterapia",
-    "terapia occupazionale",
-    "ostetricia",
-    "tecniche radiologiche",
-    "giurisprudenza",
-    "economia",
-    "ingegneria",
-    "lingua",
-    "ielts"
-]
 
 url = "https://web.uniroma1.it/trasparenza/bandi_concorso_docenti/66?field_user_centro_spesa_ugov_tid=All&field_data_pubblicazione_value%5Bvalue%5D%5Byear%5D=&field_bis_sc_e_ssd_tid=All&keys=&field_bis_gsd_ssd_target_id=All"
 
@@ -47,77 +7,12 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-strongs = soup.find_all("strong")
+links = soup.find_all("a")
 
-oggi = datetime.now()
+for link in links:
 
-print("\n=== BANDI NON SCADUTI E PERTINENTI ===\n")
+    href = link.get("href")
 
-for i in range(len(strongs)):
+    if href and ".pdf" in href.lower():
 
-    titolo = strongs[i].get_text(" ", strip=True)
-
-    if titolo in [
-        "",
-        "Data pubblicazione bando:",
-        "Data scadenza bando:",
-        "codice bando:",
-        "Filtra",
-        "Bandi di concorso pubblicati antecedentemente all'8 febbraio 2016"
-    ]:
-        continue
-
-    try:
-
-        data_scadenza = strongs[i + 3].parent.get_text(
-            " ",
-            strip=True
-        ).replace(
-            "Data scadenza bando:",
-            ""
-        ).strip()
-
-        data_scadenza_dt = datetime.strptime(
-            data_scadenza,
-            "%d-%m-%Y"
-        )
-
-        if data_scadenza_dt < oggi:
-            continue
-
-        titolo_lower = titolo.lower()
-
-        escluso = False
-
-        for parola in ESCLUSIONI:
-            if parola in titolo_lower:
-                escluso = True
-
-        if escluso:
-            continue
-
-        priorita = None
-
-        for parola in PRIORITA_ALTA:
-            if parola in titolo_lower:
-                priorita = "ALTA"
-
-        if priorita is None:
-            for parola in PRIORITA_MEDIA:
-                if parola in titolo_lower:
-                    priorita = "MEDIA"
-
-        if priorita is None:
-            for parola in PRIORITA_BASSA:
-                if parola in titolo_lower:
-                    priorita = "BASSA"
-
-        if priorita:
-
-            print(f"[{priorita}]")
-            print(titolo)
-            print("Scadenza:", data_scadenza)
-            print()
-
-    except Exception:
-        pass
+        print(href)
