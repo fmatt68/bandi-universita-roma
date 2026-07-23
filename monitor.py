3504863613600de158df680943efa2037a220b17
fmatt68 <fmattei68@hotmail.com>
2026-07-23 12:43:03 +0200

    Update email print statements in monitor.py

--- monitor.py
+++ monitor.py
@@ -3,5 +3,7 @@ import os
 print("Test GitHub")
 
 email = os.environ.get("EMAIL_ADDRESS")
+password = os.environ.get("EMAIL_PASSWORD")
 
-print("Email configurata:", email)
+print("EMAIL_ADDRESS:", email)
+print("EMAIL_PASSWORD presente:", password is not None)
