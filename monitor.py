ca3ec6ce47402d87075d9641e7dc0151aa256cec
fmatt68 <fmattei68@hotmail.com>
2026-07-23 14:40:46 +0200

    Update monitor.py to check for IELTS and deadline
    Refactor code to check for specific phrases in response text.

--- monitor.py
+++ monitor.py
@@ -1,5 +1,4 @@
 import requests
-from bs4 import BeautifulSoup
 
 url = "https://web.uniroma1.it/trasparenza/bandi_concorso_docenti/66?field_user_centro_spesa_ugov_tid=All&field_data_pubblicazione_value%5Bvalue%5D%5Byear%5D=&field_bis_sc_e_ssd_tid=All&keys=&field_bis_gsd_ssd_target_id=All"
 
@@ -7,12 +6,14 @@ response = requests.get(url)
 
 print("Status code:", response.status_code)
 
-soup = BeautifulSoup(response.text, "html.parser")
+testo = response.text
 
-links = soup.find_all("a")
+print(
+    "IELTS presente:",
+    "Instructor per corsi di preparazione esami IELTS" in testo
+)
 
-print("Numero link trovati:", len(links))
-
-for i, link in enumerate(links[:20]):
-    testo = link.get_text(strip=True)
-    print(i, testo)
+print(
+    "Data scadenza presente:",
+    "Data scadenza bando" in testo
+)
