1f2f1bf32d0592372091e667d0a0ac8fc3edb54d
fmatt68 <fmattei68@hotmail.com>
2026-07-23 16:31:25 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import re
 
 url = "https://web.uniroma1.it/trasparenza/dettaglio_bando_albo/245584"
 
@@ -10,16 +9,7 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 for a in soup.find_all("a"):
 
-    valore = str(a.get("href"))
+    testo = str(a)
 
-    if "sites/default/files" not in valore:
-        continue
-
-    match = re.search(
-        r'href="(https://web\.uniroma1\.it/trasparenza/sites/default/files/[^"]+\.pdf)"',
-        valore
-    )
-
-    if match:
-        print("PDF PULITO:")
-        print(match.group(1))
+    if "Instructor_IELTS" in testo:
+        print(testo)
