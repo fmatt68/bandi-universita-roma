1a32f4d39255c3b96373a3276316142fcb0e4fd0
fmatt68 <fmattei68@hotmail.com>
2026-07-23 15:25:35 +0200

    Refactor monitor.py to check for keywords in response
    Refactored the code to check for specific PDF links and keywords in the response text instead of using BeautifulSoup for parsing.

--- monitor.py
+++ monitor.py
@@ -1,18 +1,12 @@
 import requests
-from bs4 import BeautifulSoup
 
 url = "https://web.uniroma1.it/trasparenza/bandi_concorso_docenti/66?field_user_centro_spesa_ugov_tid=All&field_data_pubblicazione_value%5Bvalue%5D%5Byear%5D=&field_bis_sc_e_ssd_tid=All&keys=&field_bis_gsd_ssd_target_id=All"
 
 response = requests.get(url)
 
-soup = BeautifulSoup(response.text, "html.parser")
+testo = response.text.lower()
 
-links = soup.find_all("a")
-
-for link in links:
-
-    href = link.get("href")
-
-    if href and ".pdf" in href.lower():
-
-        print(href)
+print("pdf presente:", ".pdf" in testo)
+print("sites/default/files presente:", "sites/default/files" in testo)
+print("bando presente:", "bando_7-2026" in testo)
+``
