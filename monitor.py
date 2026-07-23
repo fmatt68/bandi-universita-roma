879c0701d8226d45c4ae040fe22c828bfcabb31d
fmatt68 <fmattei68@hotmail.com>
2026-07-23 15:31:39 +0200

    Refactor monitor.py to use BeautifulSoup for parsing
    Updated the script to use BeautifulSoup for parsing HTML and extracting links.

--- monitor.py
+++ monitor.py
@@ -1,11 +1,21 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://web.uniroma1.it/trasparenza/bandi_concorso_docenti/66?field_user_centro_spesa_ugov_tid=All&field_data_pubblicazione_value%5Bvalue%5D%5Byear%5D=&field_bis_sc_e_ssd_tid=All&keys=&field_bis_gsd_ssd_target_id=All"
 
 response = requests.get(url)
 
-testo = response.text.lower()
+soup = BeautifulSoup(response.text, "html.parser")
 
-print("pdf presente:", ".pdf" in testo)
-print("sites/default/files presente:", "sites/default/files" in testo)
-print("bando presente:", "bando_7-2026" in testo)
+links = soup.find_all("a")
+
+for i, link in enumerate(links[:100]):
+
+    testo = link.get_text(" ", strip=True)
+
+    href = link.get("href")
+
+    if testo and href:
+        print("-----")
+        print("TESTO:", testo)
+        print("LINK:", href)
