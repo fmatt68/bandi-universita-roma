65d75e8e57989cc107408a3e3db2f15f38760a33
fmatt68 <fmattei68@hotmail.com>
2026-07-23 15:33:36 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,21 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://web.uniroma1.it/trasparenza/bandi_concorso_docenti/66?field_user_centro_spesa_ugov_tid=All&field_data_pubblicazione_value%5Bvalue%5D%5Byear%5D=&field_bis_sc_e_ssd_tid=All&keys=&field_bis_gsd_ssd_target_id=All"
+url = "https://web.uniroma1.it/trasparenza/dettaglio_bando_albo/245584"
 
 response = requests.get(url)
 
+print("Status code:", response.status_code)
+
 soup = BeautifulSoup(response.text, "html.parser")
 
-links = soup.find_all("a")
+testo = soup.get_text("\n")
 
-for i, link in enumerate(links[:100]):
-
-    testo = link.get_text(" ", strip=True)
-
-    href = link.get("href")
-
-    if testo and href:
-        print("-----")
-        print("TESTO:", testo)
-        print("LINK:", href)
+print(testo[:5000])
