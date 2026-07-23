c20cf283539ef1220d383ec790837d1666940c93
fmatt68 <fmattei68@hotmail.com>
2026-07-23 14:43:38 +0200

    Refactor monitor.py to use BeautifulSoup for parsing
    Refactor code to use BeautifulSoup for HTML parsing and extract text lines longer than 30 characters.

--- monitor.py
+++ monitor.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://web.uniroma1.it/trasparenza/bandi_concorso_docenti/66?field_user_centro_spesa_ugov_tid=All&field_data_pubblicazione_value%5Bvalue%5D%5Byear%5D=&field_bis_sc_e_ssd_tid=All&keys=&field_bis_gsd_ssd_target_id=All"
 
@@ -6,14 +7,14 @@ response = requests.get(url)
 
 print("Status code:", response.status_code)
 
-testo = response.text
+soup = BeautifulSoup(response.text, "html.parser")
 
-print(
-    "IELTS presente:",
-    "Instructor per corsi di preparazione esami IELTS" in testo
-)
+testo = soup.get_text("\n")
 
-print(
-    "Data scadenza presente:",
-    "Data scadenza bando" in testo
-)
+righe = testo.splitlines()
+
+for riga in righe:
+    riga = riga.strip()
+
+    if len(riga) > 30:
+        print(riga)
