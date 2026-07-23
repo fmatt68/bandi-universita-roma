037f800543813abf538271245abfa67b4af2e588
fmatt68 <fmattei68@hotmail.com>
2026-07-23 15:18:19 +0200

    Applicazione filtri e priorità
    Added priority and exclusion lists for filtering relevant announcements. Updated logic to classify announcements based on priority and exclude certain titles.

--- monitor.py
+++ monitor.py
@@ -2,6 +2,45 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
+PRIORITA_ALTA = [
+    "ematologia",
+    "oncologia",
+    "immunologia",
+    "genetica",
+    "genetica medica",
+    "biologia",
+    "biologia molecolare",
+    "biologia cellulare",
+    "scienze biologiche",
+    "biotecnologie"
+]
+
+PRIORITA_MEDIA = [
+    "biochimica",
+    "patologia generale",
+    "medicina di laboratorio",
+    "tecniche di laboratorio biomedico"
+]
+
+PRIORITA_BASSA = [
+    "reumatologia",
+    "fisiologia",
+    "istologia"
+]
+
+ESCLUSIONI = [
+    "infermieristica",
+    "fisioterapia",
+    "terapia occupazionale",
+    "ostetricia",
+    "tecniche radiologiche",
+    "giurisprudenza",
+    "economia",
+    "ingegneria",
+    "lingua",
+    "ielts"
+]
+
 url = "https://web.uniroma1.it/trasparenza/bandi_concorso_docenti/66?field_user_centro_spesa_ugov_tid=All&field_data_pubblicazione_value%5Bvalue%5D%5Byear%5D=&field_bis_sc_e_ssd_tid=All&keys=&field_bis_gsd_ssd_target_id=All"
 
 response = requests.get(url)
@@ -12,7 +51,7 @@ strongs = soup.find_all("strong")
 
 oggi = datetime.now()
 
-print("\nBANDI NON SCADUTI\n")
+print("\n=== BANDI NON SCADUTI E PERTINENTI ===\n")
 
 for i in range(len(strongs)):
 
@@ -43,10 +82,41 @@ for i in range(len(strongs)):
             "%d-%m-%Y"
         )
 
-        if data_scadenza_dt >= oggi:
+        if data_scadenza_dt < oggi:
+            continue
 
-            print("TITOLO:", titolo)
-            print("SCADENZA:", data_scadenza)
+        titolo_lower = titolo.lower()
+
+        escluso = False
+
+        for parola in ESCLUSIONI:
+            if parola in titolo_lower:
+                escluso = True
+
+        if escluso:
+            continue
+
+        priorita = None
+
+        for parola in PRIORITA_ALTA:
+            if parola in titolo_lower:
+                priorita = "ALTA"
+
+        if priorita is None:
+            for parola in PRIORITA_MEDIA:
+                if parola in titolo_lower:
+                    priorita = "MEDIA"
+
+        if priorita is None:
+            for parola in PRIORITA_BASSA:
+                if parola in titolo_lower:
+                    priorita = "BASSA"
+
+        if priorita:
+
+            print(f"[{priorita}]")
+            print(titolo)
+            print("Scadenza:", data_scadenza)
             print()
 
     except Exception:
